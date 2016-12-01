e1838a54eee3bca648713a7d7f4e339a9502ed56
Zhou-Xueying <yexingguangyu123456@outlook.com>
2016-12-01 21:53:59 +0900

    server启动类

---
+++ Hotel-parent/Hotel-server/src/main/java/org/Hotel/server/start/ServerStart.java
@@ -0,0 +1,13 @@
+package org.Hotel.server.start;
+
+import org.Hotel.server.rmi.RemoteHelper;
+
+public class ServerStart {
+	
+	public static void main(String[] args){
+		
+		RemoteHelper.init();
+		
+	}
+
+}
