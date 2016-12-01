6606b844ccb46d8d467d731e1a89eb507f0bddbe
asu <asu@asus>
2016-12-01 20:47:21 +0800

    DataHelper

--- Hotel-parent/Hotel-server/src/main/java/org/Hotel/server/datahelp/OrderDataHelper.java
+++ Hotel-parent/Hotel-server/src/main/java/org/Hotel/server/datahelp/OrderDataHelper.java
@@ -1,5 +1,12 @@
 package org.Hotel.server.datahelp;
 
+import java.util.Map;
+
+import org.Hotel.common.po.OrderPO;
+
 public interface OrderDataHelper {
 
+    public Map<Integer,OrderPO> getOrderData();
+	
+	public void updateOrderData(Map<Integer,OrderPO> map);
 }
--- Hotel-parent/Hotel-server/src/main/java/org/Hotel/server/datahelp/PromotionDataHelper.java
+++ Hotel-parent/Hotel-server/src/main/java/org/Hotel/server/datahelp/PromotionDataHelper.java
@@ -1,5 +1,12 @@
 package org.Hotel.server.datahelp;
 
+import java.util.Map;
+
+import org.Hotel.common.po.PromotionPO;
+
 public interface PromotionDataHelper {
 
+	public Map<Integer,PromotionPO> getPromotionData();
+	
+	public void updatePromotionData(Map<Integer,PromotionPO> map);
 }
