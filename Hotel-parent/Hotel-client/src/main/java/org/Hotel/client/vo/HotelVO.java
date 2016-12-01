c10cc9193ccac2915a025cb81b80d06b71ae6ad4
zwk <zwk@172.28.139.57>
2016-12-01 16:32:11 +0800

    添加了3个vo

---
+++ Hotel-parent/Hotel-client/src/main/java/org/Hotel/client/vo/HotelVO.java
@@ -0,0 +1,19 @@
+package org.Hotel.client.vo;
+
+public class HotelVO {
+         public int hotelid;
+         public String hotelname;
+         
+         public HotelVO(int Hotelid,String Hotelname){
+        	 this.hotelid=Hotelid;
+        	 this.hotelname=Hotelname;
+         }
+         
+         public int getHotelid(){
+        	 return hotelid;
+         }
+         
+         public String getHotelname(){
+        	 return hotelname;
+         }
+}
