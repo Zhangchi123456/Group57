989941a306490ff5b21f53cbf5a68bccf135e4e8
Zhou-Xueying <yexingguangyu123456@outlook.com>
2016-12-02 16:24:29 +0900

    ConnectionData

---
+++ Hotel-parent/Hotel-server/src/main/java/org/Hotel/server/dataImpl/ConnectionDataImpl.java
@@ -0,0 +1,20 @@
+package org.Hotel.server.dataImpl;
+
+import java.io.Serializable;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+import org.Hotel.common.dataService.ConnectionDataService;
+
+public class ConnectionDataImpl extends UnicastRemoteObject implements ConnectionDataService,Serializable {
+
+	protected ConnectionDataImpl() throws RemoteException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public boolean isConnected() throws RemoteException {
+		return true;
+}
+
+}
