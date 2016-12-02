4da8088d662c841bd3793215b50fae34b3971f5a
asu <asu@asus>
2016-12-02 16:09:59 +0800

    Hotel2

--- Hotel-parent/Hotel-common/src/main/java/org/Hotel/common/dataService/HotelDataService.java
+++ Hotel-parent/Hotel-common/src/main/java/org/Hotel/common/dataService/HotelDataService.java
@@ -14,17 +14,10 @@ public interface HotelDataService extends Remote{
 	
 	public boolean update(HotelPO hotelpo)throws RemoteException;
 	
-	public boolean find(HotelPO hotelpo)throws RemoteException;
-	
 	public ArrayList<HotelPO> showAll()throws RemoteException;
 	
 	public ArrayList<HotelPO> hotelShowAll(int hotel_id)throws RemoteException;
 	
 	public ArrayList<HotelPO> roomShowAll(int room_id)throws RemoteException;
-	
-	public boolean updateRoom(int room_id)throws RemoteException;
-	
-	public boolean deleteRoom(int room_id)throws RemoteException;
-	
-	public boolean updateState(String stateinfo)throws RemoteException;
+
 }
