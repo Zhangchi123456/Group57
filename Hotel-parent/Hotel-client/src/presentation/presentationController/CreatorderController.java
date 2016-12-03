2ea5f622938ee9056a13a976ddb3916d3cc49291
zwk <zwk@172.28.139.57>
2016-12-03 13:49:25 +0800

    更改了注册界面和订单填写界面

--- Hotel-parent/Hotel-client/src/presentation/presentationController/CreatorderController.java
+++ Hotel-parent/Hotel-client/src/presentation/presentationController/CreatorderController.java
@@ -2,7 +2,6 @@ package presentation.presentationController;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import Helper.UiswitchHelper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -12,10 +11,13 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
 public class CreatorderController implements Initializable{
+	 private String username,usernum,telephone,roomnum,roomtype,havechild,timebegin,timeend;
+	
+	
 	 @FXML
-	 private TextField UserName,UserNum,TelePhone;//用户名，人数，电话
+	 private TextField UserName,UserNum,TelePhone,RoomnumText;//用户名，人数，电话
 	 @FXML
-	 private ChoiceBox RoomType,child;//房间类型，有无孩子
+	 private ChoiceBox RoomType,Child;//房间类型，有无孩子
 	 @FXML
 	 private DatePicker TimeBegin,TimeEnd;//开始时间，结束时间
     @FXML
@@ -25,6 +27,14 @@ public class CreatorderController implements Initializable{
     
     @FXML
     private void ConfirmButtonClicked(ActionEvent event){
+    	username=UserName.getText();
+    	usernum=UserNum.getText();
+    	telephone=TelePhone.getText();
+    	roomnum=RoomnumText.getText();
+    	roomtype=RoomType.getValue().toString();
+    	havechild=Child.getValue().toString();
+    	timebegin=TimeBegin.getValue().toString();
+    	timeend=TimeEnd.getValue().toString();
          UiswitchHelper.getApplication().goto_confirmUi();
     }
 	@Override
--- Hotel-parent/Hotel-client/src/presentation/presentationController/RegisteruiController.java
+++ Hotel-parent/Hotel-client/src/presentation/presentationController/RegisteruiController.java
@@ -17,15 +17,20 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 
 public class RegisteruiController implements Initializable {
+	
+	 private String name,password,confirmpassword,phonenumber,birthday;
+	  
      @FXML
      private TextField NameText,PhonenumberText;
+     //                姓名，     手机号码
      
      @FXML
      private PasswordField PasswordText,PasswordconfirmText;
+     //                    密码           确认密码
      
      @FXML
      private RadioButton Member,Company;
-     
+                        
      @FXML
      private Label NameLabel,BirthdayLabel;
      
@@ -47,13 +52,28 @@ public class RegisteruiController implements Initializable {
     	 Member.setSelected(false);
     	 NameLabel.setText("企业名:        ");
     	 BirthdayLabel.setVisible(false);
-    	 BirthdayDatepicker.setVisible(false);
-    	 
-    	 
+    	 BirthdayDatepicker.setVisible(false); 	 
      }
+     
+     
+     
      @FXML
      private void CommitbuttonClicked(ActionEvent event){
-    	 
+    	 if(Member.isSelected()){
+    		 name=NameText.getText();
+    		 password=PasswordText.getText();
+    		 confirmpassword=PasswordconfirmText.getText();
+    		 phonenumber=PhonenumberText.getText();
+    		 birthday=BirthdayDatepicker.getValue().toString();
+    	 }else if(Company.isSelected()){
+    		 name=NameText.getText();
+    		 password=PasswordText.getText();
+    		 confirmpassword=PasswordconfirmText.getText();
+    		 phonenumber=PhonenumberText.getText();
+    		 birthday="null";
+    	 }else{
+    		 
+    	 }
      }
      @FXML
      private void ReturnbuttonClicked(ActionEvent event){
