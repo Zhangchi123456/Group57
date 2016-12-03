7d5184e613401f8a5d802a2839f7c8e1e46083c2
zhangchi <151250190@smail.nju.edu.cn>
2016-12-03 15:06:26 +0800

    按时

--- Hotel-parent/Hotel-client/src/presentation/presentationController/LoginController.java
+++ Hotel-parent/Hotel-client/src/presentation/presentationController/LoginController.java
@@ -7,13 +7,20 @@ import Helper.UiswitchHelper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import presentation.userui.AlertBox;
 
 
 public class LoginController implements Initializable{
@@ -24,17 +31,50 @@ public class LoginController implements Initializable{
 	private TextField PassWord;
 	@FXML
 	private void RegisterClicked(ActionEvent event){
-		UiswitchHelper.getApplication().goto_Registerui();;
+		UiswitchHelper.getApplication().goto_Registerui();
 	}
 	@FXML
-	private void SureClicked(ActionEvent event){
-		if(UserId.getText().trim().substring(0, 1).equals("1"))
+	private void SureClicked(ActionEvent event)throws Exception{
+	
+		if(UserId.getText().toString().equals("")){
+			Stage window = new Stage();
+			 window.setTitle("错误提示");
+			    //modality要使用Modality.APPLICATION_MODEL
+			   
+			    window.setMinWidth(300);
+			    window.setMinHeight(200);
+
+			    Button button = new Button("确认");
+			    button.setMinWidth(105);
+			    button.setMinHeight(40);
+			    button.setOnAction(new EventHandler<ActionEvent>(){
+					public void handle(ActionEvent event) {
+						window.close();
+						
+					}
+	        		
+	        	});
+
+			    Label label = new Label("用户名为空");
+			    
+                label.setFont(new Font(20));
+			    VBox layout = new VBox(10);
+			    layout.getChildren().addAll(label , button);
+			    layout.setSpacing(20);
+			    layout.setAlignment(Pos.CENTER);
+                
+			    Scene scene = new Scene(layout);
+			    window.setScene(scene);
+			    window.show();
+			
+		}
+		else	if(UserId.getText().trim().substring(0, 1).equals("1"))
 		UiswitchHelper.getApplication().goto_Usermainui();
-		if(UserId.getText().trim().substring(0, 1).equals("2"))
+		else    if(UserId.getText().trim().substring(0, 1).equals("2"))
 			UiswitchHelper.getApplication().goto_HotelMainui();
-		if(UserId.getText().trim().substring(0, 1).equals("3"))
+		else	if(UserId.getText().trim().substring(0, 1).equals("3"))
 			UiswitchHelper.getApplication().goto_UserWebManagementui();
-		if(UserId.getText().trim().substring(0, 1).equals("4"))
+		else if(UserId.getText().trim().substring(0, 1).equals("4"))
 			UiswitchHelper.getApplication().goto_UserWebPromotionMainui();
 		
 	}
