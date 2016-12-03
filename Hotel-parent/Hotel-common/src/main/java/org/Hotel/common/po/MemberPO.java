c8f42b4bbed83e9b76bbf77ed968944eee98f0ad
asu <asu@asus>
2016-12-03 15:14:44 +0800

    kkkkkkkkk

--- Hotel-parent/Hotel-common/src/main/java/org/Hotel/common/po/MemberPO.java
+++ Hotel-parent/Hotel-common/src/main/java/org/Hotel/common/po/MemberPO.java
@@ -1,21 +1,29 @@
 package org.Hotel.common.po;
 
+import java.util.Date;
+
 public class MemberPO {
 
 	private int id;
 	
 	private String password;
 	
+	private String connection;
+	
+	private Date birthday;
+	
 	public MemberPO() {
 		super();
 	}
 	
-	public  MemberPO(int id, String password) {
+	public MemberPO(int id, String password, String connection, Date birthday) {
 		
 		super();
 		
 		this.id = id;
 		this.password = password;
+		this.connection = connection;
+		this.birthday = birthday;
 	}
 	public int getId() {
 		// TODO Auto-generated method stub
@@ -37,5 +45,25 @@ public class MemberPO {
 			
 		this.password = password;
 		
+	}
+	
+	public String getConnection() {
+		
+		return connection;
+	}
+	
+	public void setConnection(String connection) {
+		
+		this.connection = connection;
+	}
+	
+	public Date getBirthday() {
+		
+		return birthday;
+	}
+	
+	public void setBirthday(Date birthday){
+		
+		this.birthday = birthday;
 	}
 }
