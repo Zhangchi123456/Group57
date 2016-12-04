940fe0b6852775441f54456a5e3ad36dbd3d0e87
asu <asu@172.28.158.23>
2016-12-04 17:29:02 +0800

    杂七杂八

--- Hotel-parent/Hotel-client/src/presentation/presentationController/HotelroomInfouiController.java
+++ Hotel-parent/Hotel-client/src/presentation/presentationController/HotelroomInfouiController.java
@@ -2,8 +2,9 @@ package presentation.presentationController;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
-
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -16,6 +17,8 @@ import javafx.scene.control.Label;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import presentation.presentationController.OrderlistuiController.SimpleOrder;
+import vo.OrderVO;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TableColumn;
 
@@ -24,10 +27,10 @@ public class HotelroomInfouiController implements Initializable{
 	private Button ok,back;
 	
 	@FXML
-	private TableView table;
+	private TableView<SimpleRoom> table;
 	
 	@FXML
-	private TableColumn roomNum,RoomInfo,State,Last;
+	private TableColumn<SimpleRoom, String> roomNum,RoomInfo,State,Last;
 	
 	@FXML
 	private Label roomList;
@@ -46,5 +49,86 @@ public class HotelroomInfouiController implements Initializable{
 		// TODO Auto-generated method stub
 		
 	}
+	
+	public static class SimpleRoom {		 		         
+        SimpleStringProperty roomnum;
+        SimpleStringProperty roominfo;
+        SimpleStringProperty state;
+        SimpleStringProperty last;
+ 
+       private SimpleRoom(String roomnum, String roominfo, String state, String last) {
+       	 this.roomnum = new SimpleStringProperty(roomnum);
+       	  this.roominfo = new SimpleStringProperty(roominfo);
+       	  this.state = new SimpleStringProperty(state);    	  
+       	  this.last = new SimpleStringProperty(last);
+       	
+       }
+
+	/**
+	 * @return the roomnum
+	 */
+	public SimpleStringProperty getRoomnum() {
+		return roomnum;
+	}
+
+	/**
+	 * @param roomnum the roomnum to set
+	 */
+	public void setRoomnum(SimpleStringProperty roomnum) {
+		this.roomnum = roomnum;
+	}
+
+	/**
+	 * @return the roominfo
+	 */
+	public SimpleStringProperty getRoominfo() {
+		return roominfo;
+	}
+
+	/**
+	 * @param roominfo the roominfo to set
+	 */
+	public void setRoominfo(SimpleStringProperty roominfo) {
+		this.roominfo = roominfo;
+	}
+
+	/**
+	 * @return the state
+	 */
+	public SimpleStringProperty getState() {
+		return state;
+	}
+
+	/**
+	 * @param state the state to set
+	 */
+	public void setState(SimpleStringProperty state) {
+		this.state = state;
+	}
+
+	/**
+	 * @return the last
+	 */
+	public SimpleStringProperty getLast() {
+		return last;
+	}
+
+	/**
+	 * @param last the last to set
+	 */
+	public void setLast(SimpleStringProperty last) {
+		this.last = last;
+	}  
+     
+	}
+	
+	public void  Roomlist(ArrayList<RoomVO> orderlist){
+    	ObservableList<SimpleRoom> temp =FXCollections.observableArrayList();
+    	for(int i=0;i<orderlist.size();i++){
+    	temp.add(new SimpleRoom(orderlist.get(i).getHotelid(),orderlist.get(i).getId(),orderlist.get(i).getIntime()+"——"
+   		+orderlist.get(i).getOuttime(),orderlist.get(i).getPrice(),
+   		String.valueOf(orderlist.get(i).getSingleRoom()+orderlist.get(i).getFamilyRoom()+orderlist.get(i).getStandardRoom()+orderlist.get(i).getSuiteRoom()),
+   		orderlist.get(i).getComment(),orderlist.get(i).getState()));
+    	}
 
 }
