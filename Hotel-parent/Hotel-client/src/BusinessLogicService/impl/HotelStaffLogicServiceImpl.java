5d7f774cbffbec10b1baba637983cfd7eb4857f6
asu <asu@172.26.126.97>
2016-12-06 22:16:27 +0800

    HotelStaff

---
+++ Hotel-parent/Hotel-client/src/BusinessLogicService/impl/HotelStaffLogicServiceImpl.java
@@ -0,0 +1,12 @@
+package BusinessLogicService.impl;
+
+import org.Hotel.common.dataService.HotelDataService;
+
+import BusinessLogicService.Service.HotelStaffLogicService;
+
+public class HotelStaffLogicServiceImpl implements HotelStaffLogicService {
+
+	HotelDataService hds;
+	
+	
+}
