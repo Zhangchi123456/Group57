05213311cb172ebe5f4016f398c0ecf6c373096c
zhangchi <151250190@smail.nju.edu.cn>
2016-12-09 18:20:35 +0800

    啊

--- Hotel-parent/Hotel-client/src/presentation/presentationController/CreatorderController.java
+++ Hotel-parent/Hotel-client/src/presentation/presentationController/CreatorderController.java
@@ -2,6 +2,8 @@ package presentation.presentationController;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import Controller.ReservationController;
 import Helper.UiswitchHelper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -42,9 +44,35 @@ public class CreatorderController implements Initializable{
     	
     	
     	roomtype=RoomType.getValue().toString();
+    	if("单人房".equals(roomtype)){
+    		vo.setSingleRoom(roomnum);
+    		vo.setFamilyRoom("0");
+    		vo.setStandardRoom("0");
+    		vo.setSuiteRoom("0");
+    	}
+    	if("标准间".equals(roomtype)){
+    		vo.setSingleRoom("0");
+    		vo.setFamilyRoom("0");
+    		vo.setStandardRoom(roomnum);
+    		vo.setSuiteRoom("0");
+    	}
+    	if("家庭房".equals(roomtype)){
+    		vo.setSingleRoom("0");
+    		vo.setFamilyRoom(roomnum);
+    		vo.setStandardRoom("0");
+    		vo.setSuiteRoom("0");
+    	}
+    	if("套间".equals(roomtype)){
+    		vo.setSingleRoom("0");
+    		vo.setFamilyRoom("0");
+    		vo.setStandardRoom("0");
+    		vo.setSuiteRoom(roomnum);
+    	}
+    	
     	vo.setHavekids(Child.getValue().toString());
     	vo.setStarttime(TimeBegin.getValue().toString());
     	vo.setLasttime(TimeEnd.getValue().toString());
+    	
          UiswitchHelper.getApplication().goto_confirmUi();
     	}
     }
