0bc541c6bf4356b5577dbfb22bd5c2ef023df50f
Zhou-Xueying <yexingguangyu123456@outlook.com>
2016-12-11 22:14:55 +0900

    Junit

---
+++ Hotel-parent/Hotel-client/src/test/PromotionLogicServiceImplTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import BusinessLogicService.impl.PromotionLogicServiceImpl;
+import junit.framework.TestCase;
+import vo.HotelPromotionVO;
+
+public class PromotionLogicServiceImplTest extends TestCase {
+	
+	PromotionLogicServiceImpl promotion = new PromotionLogicServiceImpl();
+	
+	@Test
+	public void test1(){
+		HotelPromotionVO vo = new HotelPromotionVO();
+		
+	}
+
+}
