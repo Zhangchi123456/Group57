8d159adc72fd136a64dae3d98d6fb67766c4f7ae
zwk <zwk@172.28.186.96>
2016-12-11 23:09:07 +0800

    member and reservation test

---
+++ Hotel-parent/Hotel-client/src/test/MemberLogicServiceImplTest.java
@@ -0,0 +1,40 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.rmi.RemoteException;
+import java.text.ParseException;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import BusinessLogicService.impl.MemberLogicServiceImpl;
+import vo.CreditRecordVO;
+import vo.MemberVO;
+
+public class MemberLogicServiceImplTest {
+        MemberLogicServiceImpl member=new MemberLogicServiceImpl();
+	@Test
+	public void testfindmemberbyname() throws RemoteException {
+		MemberVO vo=member.Findmemberbyname("高晓晓");
+		assertEquals("1",vo.getlevel());
+		
+	}
+	@Test
+	public void testMembershowall() throws RemoteException{
+		ArrayList<MemberVO> memberlist=member.showall();
+		boolean result = false;
+		for(int i=0;i<memberlist.size();i++){
+			if(memberlist.get(i).getname()=="高晓晓"){
+				result=true;
+			}
+		}
+		assertTrue(result);
+	}
+	@Test
+	public void testFindcreditbyname() throws RemoteException, ParseException{
+		ArrayList<CreditRecordVO> creditlist=member.FindCreditbyname("高晓晓");
+		assertEquals(2, creditlist.size());
+	}
+
+}
