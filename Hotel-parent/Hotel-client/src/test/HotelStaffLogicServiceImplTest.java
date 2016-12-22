cdeb5778480671b0270b570123add7c5de5031b2
asu <asu@172.26.46.105>
2016-12-23 01:07:44 +0800

    aaaaaaaa

--- Hotel-parent/Hotel-client/src/test/HotelStaffLogicServiceImplTest.java
+++ Hotel-parent/Hotel-client/src/test/HotelStaffLogicServiceImplTest.java
@@ -1,19 +1,11 @@
 package test;
 
 
-import static org.junit.Assert.*;
-
-import java.rmi.RemoteException;
-import java.util.ArrayList;
-
 import org.junit.Test;
 
 import BusinessLogicService.Service.HotelStaffLogicService;
-import BusinessLogicService.Service.PromotionLogicService;
 import BusinessLogicService.impl.HotelStaffLogicServiceImpl;
-import BusinessLogicService.impl.PromotionLogicServiceImpl;
 import junit.framework.TestCase;
-import vo.RoomVO;
 
 public class HotelStaffLogicServiceImplTest extends TestCase{
 
@@ -42,7 +34,7 @@ public class HotelStaffLogicServiceImplTest extends TestCase{
 	@Test
 	public void test3(){
 		try {
-			assertTrue("南行酒店".equals(hls.FindRoomByID(1).getHotelid()));
+			assertTrue("南行酒店".equals(hls.FindRoomByID("南行酒店",1).getHotelid()));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -52,7 +44,7 @@ public class HotelStaffLogicServiceImplTest extends TestCase{
 	@Test
 	public void test4(){
 		try {
-			assertTrue("南行酒店".equals(hls.FindRoomByID(1).getHotelid()));
+			assertTrue("南行酒店".equals(hls.FindRoomByID("南行酒店",1).getHotelid()));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -62,7 +54,7 @@ public class HotelStaffLogicServiceImplTest extends TestCase{
 	@Test
 	public void test5(){
 		try {
-			assertTrue("南行酒店".equals(hls.FindRoomByID(1).getHotelid()));
+			assertTrue("南行酒店".equals(hls.FindRoomByID("南行酒店",1).getHotelid()));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -93,7 +85,7 @@ public class HotelStaffLogicServiceImplTest extends TestCase{
 	@Test
 	public void test8(){
 		try {
-			assertTrue("南行酒店".equals(hls.FindRoomByID(1).getHotelid()));
+			assertTrue("南行酒店".equals(hls.FindRoomByID("南行酒店",1).getHotelid()));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -103,7 +95,7 @@ public class HotelStaffLogicServiceImplTest extends TestCase{
 	@Test
 	public void test9(){
 		try {
-			assertTrue("南行酒店".equals(hls.FindRoomByID(1).getHotelid()));
+			assertTrue("南行酒店".equals(hls.FindRoomByID("南行酒店",1).getHotelid()));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -113,7 +105,7 @@ public class HotelStaffLogicServiceImplTest extends TestCase{
 	@Test
 	public void test10(){
 		try {
-			assertTrue("南行酒店".equals(hls.FindRoomByID(1).getHotelid()));
+			assertTrue("南行酒店".equals(hls.FindRoomByID("南行酒店",1).getHotelid()));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -143,7 +135,7 @@ public class HotelStaffLogicServiceImplTest extends TestCase{
 	@Test
 	public void test13(){
 		try {
-			assertTrue("南行酒店".equals(hls.FindRoomByID(1).getHotelid()));
+			assertTrue("南行酒店".equals(hls.FindRoomByID("南行酒店",1).getHotelid()));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -153,7 +145,7 @@ public class HotelStaffLogicServiceImplTest extends TestCase{
 	@Test
 	public void test14(){
 		try {
-			assertTrue("南行酒店".equals(hls.FindRoomByID(1).getHotelid()));
+			assertTrue("南行酒店".equals(hls.FindRoomByID("南行酒店",1).getHotelid()));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -163,7 +155,7 @@ public class HotelStaffLogicServiceImplTest extends TestCase{
 	@Test
 	public void test15(){
 		try {
-			assertTrue("南行酒店".equals(hls.FindRoomByID(1).getHotelid()));
+			assertTrue("南行酒店".equals(hls.FindRoomByID("南行酒店",1).getHotelid()));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -194,7 +186,7 @@ public class HotelStaffLogicServiceImplTest extends TestCase{
 	@Test
 	public void test18(){
 		try {
-			assertTrue("南行酒店".equals(hls.FindRoomByID(1).getHotelid()));
+			assertTrue("南行酒店".equals(hls.FindRoomByID("南行酒店",1).getHotelid()));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -204,7 +196,7 @@ public class HotelStaffLogicServiceImplTest extends TestCase{
 	@Test
 	public void test19(){
 		try {
-			assertTrue("南行酒店".equals(hls.FindRoomByID(1).getHotelid()));
+			assertTrue("南行酒店".equals(hls.FindRoomByID("南行酒店",1).getHotelid()));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -214,7 +206,7 @@ public class HotelStaffLogicServiceImplTest extends TestCase{
 	@Test
 	public void test20(){
 		try {
-			assertTrue("南行酒店".equals(hls.FindRoomByID(1).getHotelid()));
+			assertTrue("南行酒店".equals(hls.FindRoomByID("南行酒店",1).getHotelid()));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -245,7 +237,7 @@ public class HotelStaffLogicServiceImplTest extends TestCase{
 	@Test
 	public void test23(){
 		try {
-			assertTrue("南行酒店".equals(hls.FindRoomByID(1).getHotelid()));
+			assertTrue("南行酒店".equals(hls.FindRoomByID("南行酒店",1).getHotelid()));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -255,7 +247,7 @@ public class HotelStaffLogicServiceImplTest extends TestCase{
 	@Test
 	public void test24(){
 		try {
-			assertTrue("南行酒店".equals(hls.FindRoomByID(1).getHotelid()));
+			assertTrue("南行酒店".equals(hls.FindRoomByID("南行酒店",1).getHotelid()));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -265,7 +257,7 @@ public class HotelStaffLogicServiceImplTest extends TestCase{
 	@Test
 	public void test25(){
 		try {
-			assertTrue("南行酒店".equals(hls.FindRoomByID(1).getHotelid()));
+			assertTrue("南行酒店".equals(hls.FindRoomByID("南行酒店",1).getHotelid()));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
